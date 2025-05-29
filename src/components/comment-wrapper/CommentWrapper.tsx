84751f846a49b1a34ef2012bb4d6f354e877f8e2
O'tkir <keldibekovotkir767@gmail.com>
2025-05-29 08:25:44 +0500

    first commit

--- src/components/comment-wrapper/CommentWrapper.tsx
+++ src/components/comment-wrapper/CommentWrapper.tsx
@@ -1,28 +1,28 @@
-import React, { FC } from 'react'
-import { useDeleteCommentMutation, useUpdateCommentMutation } from '../../redux/api/comment.api'
+// import React, { FC } from 'react'
+// import { useDeleteCommentMutation, useUpdateCommentMutation } from '../../redux/api/comment.api'
 
-interface Props {
-  data: any
-}
-const CommentWrapper:FC<Props> = ({data}) => {
-  const [deleteComment] = useDeleteCommentMutation()
-  const [updateComment] = useUpdateCommentMutation()
+// interface Props {
+//   data: any
+// }
+// const CommentWrapper:FC<Props> = ({data}) => {
+//   const [deleteComment] = useDeleteCommentMutation()
+//   const [updateComment] = useUpdateCommentMutation()
 
-  // updateComment({id: 54, body: {text:"asdsad", star: 3}})
+//   // updateComment({id: 54, body: {text:"asdsad", star: 3}})
   
-  return (
-    <div>
-      {
-        data?.map((item:any)=> (
-          <div key={item.id}>
-            <h3>{item.text}</h3>
-            <button onClick={()=> deleteComment(item.id)}>delete</button>
-            <hr />  
-          </div>
-        ))
-      }
-    </div>
-  )
-}
+//   return (
+//     <div>
+//       {
+//         data?.map((item:any)=> (
+//           <div key={item.id}>
+//             <h3>{item.text}</h3>
+//             <button onClick={()=> deleteComment(item.id)}>delete</button>
+//             <hr />  
+//           </div>
+//         ))
+//       }
+//     </div>
+//   )
+// }
 
-export default React.memo(CommentWrapper)
+// export default React.memo(CommentWrapper)
