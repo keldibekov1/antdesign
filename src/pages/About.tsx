692a1570d17f7774433ab24e768016ab85ac2714
O'tkir <keldibekovotkir767@gmail.com>
2025-05-29 08:28:32 +0500

    first commit

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,31 +1,31 @@
-import React from "react";
-import {
-  useCreateRecipeMutation,
-  useDeleteRecipeMutation,
-  useGetRecipesQuery,
-} from "../redux/api/recipe.api";
+// import React from "react";
+// import {
+//   useCreateRecipeMutation,
+//   useDeleteRecipeMutation,
+//   useGetRecipesQuery,
+// } from "../redux/api/recipe.api";
 
-const About = () => {
+// const About = () => {
   
-  // const { data, isLoading, isError, error, isFetching } = useGetRecipesQuery(
-  //   {limit:5, genres: "20,18",}
-  // );
-  // const [createRecipe, { isLoading: createLoading }] = useCreateRecipeMutation();
-  // const [deleteRecipe, { isLoading: deleteLoading }] = useDeleteRecipeMutation();
+//   // const { data, isLoading, isError, error, isFetching } = useGetRecipesQuery(
+//   //   {limit:5, genres: "20,18",}
+//   // );
+//   // const [createRecipe, { isLoading: createLoading }] = useCreateRecipeMutation();
+//   // const [deleteRecipe, { isLoading: deleteLoading }] = useDeleteRecipeMutation();
 
-  // const handleSubmit = () => {
-  //   createRecipe(newRecipe)
-  // }
+//   // const handleSubmit = () => {
+//   //   createRecipe(newRecipe)
+//   // }
 
-  // const handleDelete = (id) => {
-  //   deleteRecipe(id)
-  // }
+//   // const handleDelete = (id) => {
+//   //   deleteRecipe(id)
+//   // }
 
-  return (
-    <div>
-      About
-    </div>
-  );
-};
+//   return (
+//     <div>
+//       About
+//     </div>
+//   );
+// };
 
-export default About;
+// export default About;
